e1da5e2cc5b10d2b51f11268560993bdd1feff19
bfgoh <gohbingfeng@gmail.com>
2025-05-01 13:59:04 +0800

    break up the ingestion into parts and steps

--- rest_api_pipeline.py
+++ rest_api_pipeline.py
@@ -5,8 +5,9 @@ import polars as pl
 df = pl.read_csv("MarketCap_Processed_20241210.csv")
 ticker = df.select(pl.col("Ticker"))
 list_of_tickers = ticker.to_series().to_list()
-list_of_tickers = list_of_tickers[:30]
-
+STEP = 20
+END = 200
+# print(list(range(5, 105, 5)))
 if __name__ == "__main__":
     pipeline = dlt.pipeline(
         pipeline_name="alpaca",
@@ -15,5 +16,10 @@ if __name__ == "__main__":
         progress="log",
         export_schema_path="schemas/export",
     )
-    source = alpaca_source(list_of_stocks=list_of_tickers)
-    info = pipeline.run(source)
+    for j in range(STEP, END + STEP, STEP):
+        i = j - STEP
+        list_of_tickers = ticker.to_series().to_list()
+        list_of_tickers = list_of_tickers[i:j]
+        source = alpaca_source(list_of_stocks=list_of_tickers, split_adjusted="split")
+        info = pipeline.run(source, loader_file_format="csv")
+        print(info)
